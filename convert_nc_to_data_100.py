51f59fd7dbae74ecef854c8433b9baab981d1230
He Zhang <he.zhang@utsouthwestern.edu>
2020-10-06 19:08:37 -0500

    add py

---
+++ convert_nc_to_data_100.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import sys
+import lzma
+import numpy as np
+from netCDF4 import Dataset
+
+_, fi, fo = sys.argv
+
+# Read the data
+g16nc = Dataset(fi, 'r')
+var_names = [ii for ii in g16nc.variables]
+var_name = var_names[0]
+data = g16nc.variables[var_name][:]
+
+# close file when finished
+g16nc.close()
+g16nc = None
+
+mydata = np.concatenate(data)
+
+mydata *= 100
+mydata = mydata.astype(np.uint16)
+
+with lzma.open(fo, 'wb') as f:
+    np.save(f, mydata)
