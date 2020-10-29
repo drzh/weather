618b8d3db1bdad22b01648c9d1326bd7110a1a5e
drzh <zhangheqd@gmail.com>
2020-10-28 22:41:12 -0500

    mod

--- train.py
+++ train.py
@@ -46,6 +46,15 @@ for o, a in opts:
     else:
         assert False, 'unhandled option'
 
+# Function to scale temperature (181 ~ 330) to group (0 ~ 149)
+def scale_group(x):
+    x = torch.round(x / 100 - 181)
+    if x < 0:
+        x = 0
+    elif x > 149:
+        x = 149
+    return x
+
 # Define a model
 class WLSTM(nn.Module):
     def __init__(self,
@@ -57,11 +66,11 @@ class WLSTM(nn.Module):
 
         # Conv and pooling model
         self.conv1 = nn.Conv2d(input_dim, 160, 3, padding = (1, 1))
-        self.pool1 = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(160, 256, 3, padding = (1, 1))
         self.conv3 = nn.Conv2d(256, 256, 3, padding = (1, 1))
         self.conv4 = nn.Conv2d(256, 256, 3, padding = (1, 1))
-        self.pool2 = nn.MaxPool2d(2, 2)
+        self.conv5 = nn.Conv2d(384, 2048, 1)
+        self.conv6 = nn.Conv2d(2048, 150, 1)
 
         # ConvLSTM model
         self.convlstm = ConvLSTM(input_dim = 256,
@@ -71,6 +80,20 @@ class WLSTM(nn.Module):
                                  batch_first = True,
                                  bias = True,
                                  return_all_layers = False)
+
+        # Axial self-attention model
+        self.attn1 = AxialAttention(dim = 64,
+                                    dim_index = 2,
+                                    dim_heads = 16,
+                                    num_dimensions = 2,
+                                    sum_axial_out = True)
+        
+        self.attn2 = AxialAttention(dim = 64,
+                                    dim_index = 3,
+                                    dim_heads = 16,
+                                    num_dimensions = 2,
+                                    sum_axial_out = True)
+        
     """
     Input: (b, t, c, h, w)
     """
@@ -89,8 +112,26 @@ class WLSTM(nn.Module):
 
         # b x t x 128 x 64 x 64 to get through ConvLSTM
         xb = torch.stack(xb, dim = 0)
-        res = self.convlstm(xb)
-        return res
+        layer_output, last_state = self.convlstm(xb)
+        h = last_state[0][0]
+
+        # convert b x 384 x 64 x 64 to b x 384 x 64 x 64
+        y = F.relu(self.conv5(h))
+                                    
+        # Axial self attension
+        y = self.attn1(y)
+        y = self.attn2(y)
+        y = self.attn1(y)
+        y = self.attn2(y)
+        y = self.attn1(y)
+        y = self.attn2(y)
+        y = self.attn1(y)
+        y = self.attn2(y)
+
+        # Convert to 150 x 64 x 64 to represent probability for each unit
+        y = self.conv6(y)
+        
+        return y
 
 # Create a model
 model = WLSTM()
@@ -99,16 +140,35 @@ model = WLSTM()
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
+i = 0
 for xtrain, ytrain in readdata(fl, mean, sd):
     xtrain, ytrain = torch.Tensor([xtrain]), torch.Tensor(ytrain)
     optimizer.zero_grad()
-    layer_output, last_state = model(xtrain)
-    ypred = last_state[0][0]
-    print(ypred.size())
-    print(ytrain.size())
+    ypred = model(xtrain)
+
+    # Reshape ypred
+    ypred = torch.flatten(ypred, 2)
+    ypred = torch.transpose(ypred, 1, 2)
+    ypred = torch.flatten(ypred, 0, 1)
+
+    # Reshape ytrain
+    ytrain = torch.flatten(ytrain, 1)
+    ytrain = torch.flatten(ytrain, 0, 1)
+
+    # Label ytrain from (181 ~ 330) to group (0 ~ 149)
+    ylabel = torch.Tensor([scale_group(x) for x in ytrain]).long()
+
+    loss = criterion(ypred, ylabel)
+    loss.backward()
+    optimizer.step()
+    print('echoch', i, 'loss:', loss.item())
+    i += 1
+
+    # print(ypred.size())
+    # print(ytrain.size())
     # print(layer_output[0].size())
     # print('---------')
     # print(last_state[0][0].size())
     # print('---------')
     # print(last_state[0][1].size())
-    sys.exit(0)
+    # sys.exit(0)
