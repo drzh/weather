b5fade6497ada749d412cb47d78aed5bbb119a7b
drzh <zhangheqd@gmail.com>
2020-11-01 01:18:20 -0600

    mod

--- readdata.py
+++ readdata.py
@@ -4,6 +4,8 @@
 
 import lzma
 import pickle
+import math
+import multiprocessing
 
 # Function to read file and return matrix
 def readfi(fi):
@@ -40,18 +42,64 @@ def readline(line):
         pred.append(dmat)
 
     return [train, pred]
+
+# Process lines
+def readlines(lines):
+    dout = []
+    for line in lines:
+        d = readline(line)
+        dout.append(d)
+    return dout
+
+# worker
+def worker(idx, lines, return_dict):
+    d = readlines(lines)
+    return_dict[idx] = d
+
+# Process linespt
+def readlinespt(linespt):
+    manager = multiprocessing.Manager()
+    return_dict = manager.dict()
+    jobs = []
+    dim1 = len(linespt)
+    for i in range(dim1):
+        lines = linespt[i]
+        p = multiprocessing.Process(target=worker, args=(i, lines, return_dict))
+        jobs.append(p)
+        p.start()
+
+    for p in jobs:
+        p.join()
     
-def readdata(flist, n = 1, cpu = 1):
+    dout = []
+    idxs = list(return_dict.keys())
+    idxs.sort()
+    for i in idxs:
+        dout += return_dict[i]
+    return dout
+
+# Process file list
+def readdata(flist, n = 1, threads = 1):
+    npt = math.ceil(n / threads) if threads > 1 else 1
     with open(flist, 'r') as f:
         i = 0
         dout = []
+        lines = []
+        linespt = []  # list of lines in each thread
+        proc = []
         for line in f:
-            dline = readline(line)
-            # yield output
-            dout.append(dline)
+            lines.append(line)
             i += 1
-            if i % n == 0:
-                yield dout
-                dout = []
-                i = 0
+            if i % npt == 0:
+                # Push npt lines to linespt
+                linespt.append(lines)
+                lines = []
 
+            if i % n == 0:
+                # Process n lines in threads
+                if lines:
+                    linespt.append(lines)
+                    lines = []
+                dout = readlinespt(linespt)
+                yield dout
+                linespt = []
--- train.py
+++ train.py
@@ -22,10 +22,10 @@ def eprint(msg):
 # Parsing parameters
 try :
     opts, args = getopt.getopt(sys.argv[1:],
-                               'm:s:l:o:n:b:e:',
+                               'm:s:l:o:n:b:e:t:',
                                ['mean=', 'sd=', 'list=', 'out=',
                                 'lr=', 'model=', 'dev=', 'ln=',
-                                'ld=', 'batch=', 'epoch='
+                                'ld=', 'batch=', 'epoch=', 'threads='
                                ]
     )
 except getopt.GetoptError as err :
@@ -36,6 +36,7 @@ mean = 0
 sd = 1
 bat = 1    # number of samples in each minibatch
 epo = 1    # number of epoch for each minibatch
+th = 1
 lrt = 0.001
 ld = 0.5
 ln = 0
@@ -52,6 +53,8 @@ for o, a in opts:
         sd = float(a)
     elif o in ('-b', '--batch'):
         bat = int(a)
+    elif o in ('-t', '--threads'):
+        th = int(a)
     elif o in ('-3', '--epoch'):
         epo = int(a)
     elif o in ('--lr'):
@@ -75,7 +78,7 @@ for o, a in opts:
 
 # Prepare GPU
 device = torch.device(dev if torch.cuda.is_available() else "cpu")
-print('Using', device, file = sys.stderr)
+# print('Using', device, file = sys.stderr)
 
 # Function to scale temperature (200 ~ 299K) to group (0 ~ 19)
 def scale_group(x):
@@ -103,11 +106,12 @@ optimizer = torch.optim.Adam(model.parameters(), lr = lrt)
 # optimizer = torch.optim.SGD(model.parameters(), lr=lrt, momentum=0.9)
 
 # Decays the learning rate according to epoch
-scheduler = StepLR(optimizer, step_size = ln, gamma = ld)
+scheduler = StepLR(optimizer, step_size = 1, gamma = ld)
 
 # Train the model
 i = 0
 ibat = 0
+istep = 0
 loss_sum = 0
 for din in readdata(fl, bat):
     ibat += 1
@@ -144,20 +148,19 @@ for din in readdata(fl, bat):
         # Process the epoch of minibatch
         optimizer.step()
         optimizer.zero_grad()
-        print(datetime.now().strftime('%H:%M:%S'), ' input=', i, ' batch=', ibat, ' epoch=', ep + 1, ' loss=', '%.8f' % loss_sum, sep='')
+
+        # Print epoch and loss information
+        print(datetime.now().strftime('%H:%M:%S'), ' input=', i, ' batch=', ibat, ' epoch=', ep + 1, ' lr=', optimizer.param_groups[0]['lr'], ' loss=', '%.8f' % loss_sum, sep='')
         sys.stdout.flush()
         loss_sum = 0
-        # Export model state dict every n minibatchs
-        if ibat % n == 0:
-            fout = fo + '.state_dict.' + str(ibat) + '.pt'
+
+        # Export model state dict every n inputs
+        if i % n == 0:
+            fout = fo + '.state_dict.b_' + str(ibat) + '.e_' + str(ep + 1) + '.pt'
             torch.save(model.state_dict(), fout)
-            # Print learning rate
-            if ln > 0:
-                for param_group in optimizer.param_groups:
-                    print('lr =', param_group['lr'])
 
-    # Decays the learning rate
-    if ln > 0:
-        scheduler.step()
-
+        # Decay the learning rate
+        if ln > 0 and i - istep >= ln:
+            scheduler.step()
+            istep = i
 
